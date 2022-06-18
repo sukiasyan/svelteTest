0a5757224184f7c4530e05249cd20695e9e21e57
hsukiasyan <h.sukiasyan@gethatch.com>
2022-06-18 22:36:42 +0200

    push rest files

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import FeedbackList from './components/FeedbackList.svelte';
+
+	let  feedback = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	]
+
+const deleteFeedback = (e) => {
+	const itemId = e.detail
+	feedback.filter((item) => item.id !== itemId);
+	
+}
+
+</script>
+
+<main class="container">
+<FeedbackList feedback={feedback} on:delete-feedback={deleteFeedback}/>
+</main>
